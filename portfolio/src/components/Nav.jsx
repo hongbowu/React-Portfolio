9719e61de5c9d2c26eba485ac065337857492226
hongbowu <wuhongbo87@gmail.com>
2024-02-05 20:05:10 -0500

    remove the css files

--- portfolio/src/components/Nav.jsx
+++ portfolio/src/components/Nav.jsx
@@ -1,3 +1,13 @@
 export default function Nav() {
-console.log("this is nav")
+    return(
+        <nav >
+          <a>About Me</a>
+          <a>Portfolio</a>
+          <a>Contact</a>
+          <a>Resume</a>
+        </nav>
+       
+    )
+    
+    
 }
