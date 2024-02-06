622d08db84ee54b118d3ba9312b476621d3eaca0
hongbowu <wuhongbo87@gmail.com>
2024-02-05 22:05:22 -0500

    roughly set about me page

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
+import About from './pages/About.jsx';
 import './App.css'
 
 const router = createBrowserRouter([
