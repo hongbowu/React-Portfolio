5463beaa57d16a9904032927bf04bc7a18c1c0fc
hongbowu <wuhongbo87@gmail.com>
2024-02-05 20:43:12 -0500

    roughly set header style

--- portfolio/src/components/Nav.jsx
+++ portfolio/src/components/Nav.jsx
@@ -1,11 +1,19 @@
+import '../App.css'
 export default function Nav() {
+
     return(
-        <nav >
-          <a>About Me</a>
-          <a>Portfolio</a>
-          <a>Contact</a>
-          <a>Resume</a>
-        </nav>
+        <header className="header" >
+            <h1 >
+                Hongbo
+            </h1>
+        
+            <nav className='nav'>
+            <a>About Me</a>
+            <a>Portfolio</a>
+            <a>Contact</a>
+            <a>Resume</a>
+            </nav>
+        </header>
        
     )
     
