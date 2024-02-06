bd58b6890762d2e60ed0b4663b2b438c9af7dbe7
hongbowu <wuhongbo87@gmail.com>
2024-02-05 22:07:01 -0500

    roughly set portfolio page

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
 import About from './pages/About.jsx';
+import Portfolio from './pages/Protfolio.jsx';
 import './App.css'
 
 const router = createBrowserRouter([
