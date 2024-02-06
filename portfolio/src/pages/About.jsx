148a020f5a763f24c1179b7e4467727f1a5212d6
hongbowu <wuhongbo87@gmail.com>
2024-02-05 23:10:46 -0500

    set about me page

--- portfolio/src/pages/About.jsx
+++ portfolio/src/pages/About.jsx
@@ -1,7 +1,14 @@
 export default function About() {
     return(
         <div>
-            <p>about me</p>
+            <p>I immigrated from China and have now 
+                become a proud U.S. citizen 
+                with a perfect family. 
+                Currently, I am immersed in a coding bootcamp, 
+                learning full-stack development. 
+                I aspire to secure a job 
+                in front-end web development in the future.
+            </p>
         </div>
     )
 }
--- portfolio/src/pages/Portfolio.jsx
+++ portfolio/src/pages/Portfolio.jsx
@@ -1,7 +1,19 @@
 export default function Portfolio() {
     return(
         <div>
-            <p>Portfolio</p>
+            <h2>Front-end</h2>
+            <p>HTML5</p>
+            <p>CSS</p>
+            <p>JAVASCRIPT</p>
+            <p>Jquery</p>
+            <p>Bootstrap</p>
+            <p>React</p>
+            <h2>Back-end</h2>
+            <p>NodeJs</p>
+            <p>ExpressJs</p>
+            <p>Mysql</p>
+            <p>MongoDb</p>
+            <p></p>
         </div>
     )
 }
